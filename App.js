ca407c2673f00e563f8dfa1a04b1baa100229e3a
zeev-770 <82053404+zeev-770@users.noreply.github.com>
2021-04-06 20:53:17 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+const useState = React.useState;
+const useEffect = React.useEffect;
+
+const App = () => {
+
+    return (
+        <div>
+            <input type="text"/>
+            <input type="text"/>
+        </div>
+    )
+}
